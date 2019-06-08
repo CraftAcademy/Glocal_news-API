edc585aff7eb2d8f7577b7884481b3017af42ce0
raptorf1 <raptor_f1@hotmail.com>
2019-06-08 16:29:13 +0200

    Creates serializer for users. Adds user information to index serializer for articles.

--- app/serializers/articles/index_serializer.rb
+++ app/serializers/articles/index_serializer.rb
@@ -2,4 +2,5 @@ class Articles::IndexSerializer < ActiveModel::Serializer
   attributes :id, :title, :ingress, :body, :image, :published, :created_at, :written_by, :country, :city
   has_many :reviews, serializer: Reviews::Serializer 
   belongs_to :category, serializer: Categories::IndexSerializer
+  belongs_to :user, serializer: Users::Serializer
 end
